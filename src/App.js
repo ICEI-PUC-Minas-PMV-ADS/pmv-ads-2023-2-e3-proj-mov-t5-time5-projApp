65c4f67623f06058e594d233dc0ff9ec414087f7
DavidGomes1 <114623537+DavidGomes1@users.noreply.github.com>
2023-10-15 22:07:55 -0300

    Cadastro/Login

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+import Cadastro from './src/pages/Cadastro'
+
+const App = () => {
+
+return (
+
+  <Cadastro />
+
+);
+
+}
+
+export default App;
